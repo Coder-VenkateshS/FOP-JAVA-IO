0a9472b105afafe16e743a293032c3ead184fe8b
FacePrepJFS <12venkat21@gmail.com>
2019-11-28 18:04:12 +0530

    IO completed

---
+++ facegardensjdbc/src/util/DBUtil.java
@@ -0,0 +1,127 @@
+package util;
+
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+import javax.sql.rowset.CachedRowSet;
+
+
+public class DBUtil {
+	
+	static Connection conn=null;
+	public static Properties loadPropertiesFile() throws Exception {
+
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream("resources/jdbc.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	}
+    //Connect to DB
+    public static void dbConnect() throws Exception {
+    	Properties prop = loadPropertiesFile();
+
+		final String driver = prop.getProperty("driver");
+		final String url = prop.getProperty("url");
+		final String username = prop.getProperty("username");
+		final String password = prop.getProperty("password");
+       
+		try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+      
+      
+        try {
+            conn = DriverManager.getConnection(url,username,password);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console" + e);
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    //Close Connection
+    public static void dbDisconnect() throws SQLException {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (Exception e){
+           throw e;
+        }
+    }
+
+    //DB Execute Query Operation
+    public static ResultSet dbExecuteQuery(String queryStmt) throws Exception {
+        //Declare statement, resultSet and CachedResultSet as null
+        Statement stmt = null;
+        ResultSet resultSet = null;
+        CachedRowSet crs = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            System.out.println("Select statement: " + queryStmt + "\n");
+
+            //Create statement
+            stmt = conn.createStatement();
+
+            //Execute select (query) operation
+            resultSet = stmt.executeQuery(queryStmt);
+
+            //CachedRowSet Implementation
+            //In order to prevent "java.sql.SQLRecoverableException: Closed Connection: next" error
+            //We are using CachedRowSet
+         //   crs = new CachedRowSet();
+            crs.populate(resultSet);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                //Close resultSet
+                resultSet.close();
+            }
+            if (stmt != null) {
+                //Close Statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+        //Return CachedRowSet
+        return crs;
+    }
+
+    //DB Execute Update (For Update/Insert/Delete) Operation
+    public static void dbExecuteUpdate(String sqlStmt) throws Exception {
+        //Declare statement as null
+        Statement stmt = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            //Create Statement
+            stmt = conn.createStatement();
+            //Run executeUpdate operation with given sql statement
+            stmt.executeUpdate(sqlStmt);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeUpdate operation : " + e);
+            throw e;
+        } finally {
+            if (stmt != null) {
+                //Close statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+    }
+}
+
